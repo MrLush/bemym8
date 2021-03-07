254917e418dace0257fea93cfbc225ab1f884990
mrlush <lushnikv18@gmail.com>
2021-03-07 16:26:41 +0300

    Init project with spring initializr.

---
+++ src/main/java/com/bemym8/BeMyM8Application.java
@@ -0,0 +1,13 @@
+package com.bemym8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMyM8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeMyM8Application.class, args);
+	}
+
+}
