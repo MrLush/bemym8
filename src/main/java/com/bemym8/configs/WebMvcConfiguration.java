d9e26094b33e0db9089dd6e28e6a03d2edba8609
Daniil Lushnikov <lushnikv18@gmail.com>
2021-03-24 20:25:37 +0300

    Update WebMvcConfiguration.java

--- src/main/java/com/bemym8/configs/WebMvcConfiguration.java
+++ src/main/java/com/bemym8/configs/WebMvcConfiguration.java
@@ -5,20 +5,22 @@ import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-/*
+
 @Configuration
 @Import({ WebSecurityConfiguration.class })
 public class WebMvcConfiguration implements WebMvcConfigurer {
     //TODO Here we can add paths to files in our projects
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry){
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/");
-    }
-
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
     }
+    
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+             "classpath:/resources/", "classpath:/static/"};
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
 }
-*/
