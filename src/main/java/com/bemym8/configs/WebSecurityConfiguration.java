0b7262ca1db5f28c249b0bc8d064347e62ce8b1d
Daniil Lushnikov <lushnikv18@gmail.com>
2021-03-24 18:12:22 +0300

    Changed permissions

--- src/main/java/com/bemym8/configs/WebSecurityConfiguration.java
+++ src/main/java/com/bemym8/configs/WebSecurityConfiguration.java
@@ -38,7 +38,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/projects","/community","/registration","/static/**").permitAll()
+                .antMatchers("/","/projects","/community","/registration","/static/").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
