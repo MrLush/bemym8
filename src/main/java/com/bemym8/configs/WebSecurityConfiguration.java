17c77b6a8f94828cc9d81ad8d258aee436af0d59
Daniil Lushnikov <lushnikv18@gmail.com>
2021-03-24 21:35:00 +0300

    Changed paths

--- src/main/java/com/bemym8/configs/WebSecurityConfiguration.java
+++ src/main/java/com/bemym8/configs/WebSecurityConfiguration.java
@@ -39,7 +39,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/projects","/community","/registration","/resources/**").permitAll()
+                .antMatchers("/","/projects","/community","/registration").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
