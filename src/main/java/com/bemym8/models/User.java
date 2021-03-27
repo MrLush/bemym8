743bae365b2dc1b1d083ca84f1aa91ed46fb7b93
Daniil Lushnikov <lushnikv18@gmail.com>
2021-03-28 00:13:30 +0300

    Update User.java

--- src/main/java/com/bemym8/models/User.java
+++ src/main/java/com/bemym8/models/User.java
@@ -27,9 +27,9 @@ public class User {
     public void setId(Long id) {
         this.id = id;
     }
-
-    public String getUsername() {
-        return username;
+//TODO change it
+    public Long getUsername() {
+        return id;
     }
 
     public void setUsername(String username) {
