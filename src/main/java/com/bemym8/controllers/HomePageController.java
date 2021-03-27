605347901bed6c8e96f1da5d562080d5f1554949
Daniil Lushnikov <lushnikv18@gmail.com>
2021-03-27 22:27:03 +0300

    revert

--- src/main/java/com/bemym8/controllers/HomePageController.java
+++ src/main/java/com/bemym8/controllers/HomePageController.java
@@ -1,11 +1,5 @@
 package com.bemym8.controllers;
 
-import com.bemym8.models.User;
-import com.bemym8.repo.ProjectRepository;
-import com.bemym8.repo.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,13 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomePageController {
 
-    @Autowired
-    private UserRepository userRepository;
-
     @GetMapping("/")
-    public String homePage(Model model,@AuthenticationPrincipal UserDetails currentUser){
-        User user = (User) userRepository.findByUsername(currentUser.getUsername());
-        model.addAttribute("currentStudent", user);
+    public String homePage(Model model){
         model.addAttribute("title","BeMyM8 - Home page");
         return "home";
     }
