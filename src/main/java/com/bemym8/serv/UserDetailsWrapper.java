49f2a8afd4e45add8161b23a1358458bc5b5f6bd
Daniil Lushnikov <lushnikv18@gmail.com>
2021-03-27 23:32:19 +0300

    test

--- src/main/java/com/bemym8/serv/UserDetailsWrapper.java
+++ src/main/java/com/bemym8/serv/UserDetailsWrapper.java
@@ -13,16 +13,14 @@ import java.util.Set;
 
 public class UserDetailsWrapper implements UserDetails {
     private User user;
-    private Long id;
+
     public UserDetailsWrapper(User user){
         this.user = user;
-        this.id = user.getId();
     }
 
     public Long getId() {
-        return id;
+        return this.user.getId();
     }
-
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
