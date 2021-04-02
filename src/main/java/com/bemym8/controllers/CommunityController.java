afd9c5db650dc6ff31e74c51f92295c75e7a8db8
Daniil Lushnikov <lushnikv18@gmail.com>
2021-04-02 11:33:39 +0300

    Add page support

--- src/main/java/com/bemym8/controllers/CommunityController.java
+++ src/main/java/com/bemym8/controllers/CommunityController.java
@@ -1,13 +1,22 @@
 package com.bemym8.controllers;
 
+import com.bemym8.models.Project;
+import com.bemym8.repo.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CommunityController {
+    
+    @Autowired
+    private ProjectRepository projectRepository;
+
     @GetMapping("/community")
     public String projectsPage(Model model){
+        Iterable<Project> project = projectRepository.findAllByOrderByIdAsc();
+        model.addAttribute("project", project);
         model.addAttribute("title","BeMyM8 - Community");
         return "community";
     }
