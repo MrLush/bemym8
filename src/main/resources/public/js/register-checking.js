8a1b6c3cbc3f289483703f70a357670a5a5e3474
Daniil Lushnikov <lushnikv18@gmail.com>
2021-04-02 14:13:09 +0300

    front changes

--- src/main/resources/public/js/register-checking.js
+++ src/main/resources/public/js/register-checking.js
@@ -1,3 +1,8 @@
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 function check_pass() {
     if (document.getElementById('password').value ==
             document.getElementById('confirm_password').value) {
