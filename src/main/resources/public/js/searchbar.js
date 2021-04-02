a7e859a1d345332b9501c9931c83aed66aa130b7
Daniil Lushnikov <lushnikv18@gmail.com>
2021-04-02 19:46:47 +0300

    searchbar and fix

---
+++ src/main/resources/public/js/searchbar.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  document.getElementById('btn').onclick = function()
+  {
+      if(document.getElementById("searchRequest").value){
+        window.location.href = "https://bemym8.herokuapp.com/search?username=" + document.getElementById("searchRequest").value
+      }
+      else{
+        window.location.href = "https://bemym8.herokuapp.com/"
+      }
+  }
+  ;
+}
