10d68266758fa9bcefdd8128b2dabda1d3df7167
Daniil Lushnikov <lushnikv18@gmail.com>
2021-04-03 18:21:05 +0300

    Update searchbar.js

--- src/main/resources/public/js/searchbar.js
+++ src/main/resources/public/js/searchbar.js
@@ -2,10 +2,10 @@ window.onload = function() {
   document.getElementById('btn').onclick = function()
   {
       if(document.getElementById("searchRequest").value){
-        window.location.href = "https://bemym8.herokuapp.com/search?searchRequest=" + document.getElementById("searchRequest").value
+        window.location.href = "https://bemym8.herokuapp.com/search/" + document.getElementById("searchRequest").value
       }
       else{
-        window.location.href = "https://bemym8.herokuapp.com/"
+        window.location.href = "https://bemym8.herokuapp.com"
       }
   };
 }
