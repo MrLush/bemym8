93c821f0dbefe78654c1d326f0c6d3c4dd18e1b3
Daniil Lushnikov <lushnikv18@gmail.com>
2021-04-03 03:18:24 +0300

    Update AccountController.java

--- src/main/java/com/bemym8/controllers/AccountController.java
+++ src/main/java/com/bemym8/controllers/AccountController.java
@@ -33,7 +33,7 @@ public class AccountController {
     }
 
     @PostMapping("/account")
-    public String accountPageSubmit(@RequestParam String username, @RequestParam String firstName, @RequestParam String lastName, @RequestParam String body, Model model){
+    public String accountPageSubmit(@RequestParam String username, @RequestParam String firstName, @RequestParam String lastName, Model model){
         try{
             User user = userRepository.findByUsername(username);
             user.setFirstName(firstName);
